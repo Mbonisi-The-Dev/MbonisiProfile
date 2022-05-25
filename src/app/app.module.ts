17f97b7886064de9556a5e3e93af7a783854cde5
mbonisi tshuma <mbonisitshuma287@gmail.com>
2022-05-25 15:21:55 +0200

    Day One Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
